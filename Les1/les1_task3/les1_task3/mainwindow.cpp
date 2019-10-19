23f4ced152b44807a2c737fc96362aa280e4d029
krilatiy <pos@list.ru>
2019-10-19 20:22:33 +0300

    les1: 1.3

---
+++ Les1/les1_task3/les1_task3/mainwindow.cpp
@@ -0,0 +1,31 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+/**Создать программу, разместить на поле виджет QPlainTextEdit.
+ * Добавить кнопки: для добавления фиксированной строки, замены на готовый текст.
+ * Разместить QTextEdit. Через метод setHtml(QString) установите текст произвольного
+ * html-кода, например <font color='red'>Hello</font>.*/
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_btn_add_clicked()
+{
+    QString s = ui->plainTextEdit->toPlainText();
+    ui->textEdit->setPlainText(s);
+}
+
+void MainWindow::on_btn_replace_clicked()
+{
+    QString s = ui->textEdit->toPlainText();
+    ui->textEdit->setHtml(s);
+}
