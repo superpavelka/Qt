9b69d4720c085b33e63d2aed7fc8d17342bf8925
krilatiy <pos@list.ru>
2019-11-08 15:59:17 +0300

    les4: 4.2 override context menu in main window

---
+++ Les4/les4_task2/mainwindow.cpp
@@ -0,0 +1,191 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QFile>
+#include <QFileDialog>
+#include <QMessageBox>
+
+/*
+Создать фильтр в текстовом редакторе, перехватывающий вызовы контекстного меню.
+Добавить режим «Открыть только для чтения», блокирующий любые правки.
+*/
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->plainTextEdit_2->setReadOnly(true);
+    m_menu = new QMenu( this );
+    roAction = m_menu->addAction( "Режим только чтение(ВЫКЛ)" );
+    connect( roAction, SIGNAL( triggered() ), this, SLOT( read_only_switch() ) );
+    QFile file ("./lasttext.txt");
+    if (file.open(QIODevice::ReadOnly))
+    {
+        QByteArray byteArray = file.readAll(); // считываем весь файл
+        ui->plainTextEdit->setPlainText(tr(byteArray.data()));
+        ui->plainTextEdit_2->setPlainText("lasttext.txt");
+        file.close();
+    }
+
+    edit = true;
+    read_only = false;
+}
+
+MainWindow::~MainWindow()
+{
+    QFile file ("./lasttext.txt");
+
+    if (file.open(QIODevice::WriteOnly))
+    {
+        QString s = ui->plainTextEdit->toPlainText();
+        QByteArray barr = s.toUtf8();          // преобразуем строку в
+                                               // последовательность байт
+                                               // кодировка текста UTF-8
+        file.write(barr, barr.length());       // записываем
+        file.close();
+    }
+
+    delete ui;
+}
+
+void MainWindow::on_btn_save_clicked()
+{
+  QString filename = QFileDialog::getSaveFileName(this, "Пример фильтра",
+                                   QDir::currentPath(),"Текстовый файл(*.txt);;Двоичный файл(*.original)");
+  if (filename.length())
+  {
+      QString ext = QString(&(filename.data()[filename.length() - 9]));
+      QFile file (filename);
+
+      if (file.open(QIODevice::WriteOnly))
+      {
+          QString s = ui->plainTextEdit->toPlainText();
+          QByteArray barr = s.toUtf8();          // преобразуем строку в
+                                                 // последовательность байт
+                                                 // кодировка текста UTF-8
+          file.write(barr, barr.length());       // записываем
+          if (ext == ".original")
+          {
+              s = "Author:" + ui->lineEdit->text();
+              barr = s.toUtf8();
+              file.write(barr, barr.length());
+          }
+          file.close();
+      }
+  }
+}
+
+void MainWindow::on_btn_open_clicked()
+{
+  QString filename = QFileDialog::getOpenFileName(this, "Пример фильтра",
+                                   QDir::currentPath(),"Текстовый файл(*.txt);;Двоичный файл(*.original)");
+  if (filename.length())
+  {
+      QString ext = QString(&(filename.data()[filename.length() - 9]));
+      QFile file (filename);
+
+      if (file.open(QIODevice::ReadOnly))
+      {
+          QByteArray byteArray = file.readAll(); // считываем весь файл
+          QString s = tr(byteArray.data());
+
+          if (ext == ".original")
+          {
+            QStringList list = s.split("Author:");
+            ui->plainTextEdit->setPlainText(list[0]);
+            if (list.size() > 1)
+                ui->lineEdit->setText(list[1]);
+            else
+                ui->lineEdit->setText("");
+          }
+          else
+          {
+              ui->plainTextEdit->setPlainText(s);
+              ui->lineEdit->setText("");
+          }
+          ui->plainTextEdit_2->setPlainText(filename);
+          file.close();
+      }
+
+  }
+}
+
+
+void MainWindow::on_btn_undo_clicked()
+{
+    edit = false;
+    if (cur_undo_pos > 0)
+    {
+        cur_undo_pos--;
+        QString s = undo_list.at(cur_undo_pos);
+        ui->plainTextEdit->setPlainText(s);
+    }
+
+}
+
+
+void MainWindow::on_btn_redo_clicked()
+{
+    edit = false;
+    if (undo_list.size() - 1 >= (cur_undo_pos + 1))
+    {
+        cur_undo_pos++;
+        QString s = undo_list.at(cur_undo_pos);
+        ui->plainTextEdit->setPlainText(s);
+    }
+
+
+}
+
+void MainWindow::on_desc_btn_clicked()
+{
+    QMessageBox msgBox;
+    msgBox.setIcon(QMessageBox::Information);
+    QFile file (":/desc/description.txt");
+    file.open(QIODevice::ReadOnly);
+    QByteArray byteArray = file.readAll();
+    msgBox.setText(tr(byteArray.data()));
+    msgBox.exec();
+}
+
+void MainWindow::on_plainTextEdit_textChanged()
+{
+    if (edit)
+    {
+        QString s = ui->plainTextEdit->toPlainText();
+        if (!undo_list.isEmpty() && undo_list.last() != s)
+        {
+            undo_list.append(s);
+            cur_undo_pos++;
+        }
+        else if (undo_list.isEmpty())
+        {
+            undo_list.append(s);
+            cur_undo_pos = 0;
+        }
+    }
+    edit = true;
+}
+
+void MainWindow::contextMenuEvent( QContextMenuEvent* e ) {
+    if( m_menu ) {
+        m_menu->exec( e->globalPos() );
+    }
+}
+
+void MainWindow::read_only_switch()
+{
+    if (!read_only)
+    {
+        read_only = true;
+        roAction->setText("Режим только чтение(ВКЛ)");
+        ui->plainTextEdit->setReadOnly(true);
+    }
+    else
+    {
+        read_only = false;
+        roAction->setText("Режим только чтение(ВЫКЛ)");
+        ui->plainTextEdit->setReadOnly(false);
+    }
+}
