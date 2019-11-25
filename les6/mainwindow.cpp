d325bee93f5dd0b624006d50b83580f5bf521648
krilatiy <pos@list.ru>
2019-11-25 19:24:41 +0300

    les6: 6.1, 6.2(without indentation and page width), 6.3

--- les6/mainwindow.cpp
+++ les6/mainwindow.cpp
@@ -6,7 +6,8 @@
 #include <QMessageBox>
 #include <QPrinter>
 #include <QPrintDialog>
-
+#include <QPainter>
+#include <QInputDialog>
 /*
 Добавить меню в текстовый редактор.
 Добавить в текстовый редактор печать, класс печати. Учтите, что строка может не помещаться по ширине страницы. Реализовать разбиение текста на дополнительные строки.
@@ -21,22 +22,34 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);    
     ui->plainTextEdit->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     ui->centralwidget->setLayout(ui->verticalLayout);
-    QPlainTextEdit* pte = ui->plainTextEdit;
     QMenu *m_menu = new QMenu(this);
     ui->btn_menu->setMenu(m_menu);
+    htmlMode = false;
+    parMode = false;
     roAct = new QAction("Только чтение(ВКЛ)", m_menu);
     printAct = new QAction("Печать", m_menu);
+    hrAct = new QAction("Горизонтальная линия при переходе на новую строку(ВКЛ)", m_menu);
+    parAct = new QAction("Нумерация абзацев(ВКЛ)", m_menu);
+    tableInsAct = new QAction("Вставка таблицы", m_menu);
+
     m_menu->addAction(roAct);
     m_menu->addAction(printAct);
-    //pte->setContextMenuPolicy(Qt::ActionsContextMenu);
-    pte->connect(roAct, SIGNAL(triggered()), this, SLOT(read_only_switch()));
-    pte->connect(printAct, SIGNAL(triggered()), this, SLOT(print()));
+    m_menu->addAction(hrAct);
+    m_menu->addAction(parAct);
+    m_menu->addAction(tableInsAct);
+
+    connect(roAct, SIGNAL(triggered()), this, SLOT(read_only_switch()));
+    connect(printAct, SIGNAL(triggered()), this, SLOT(print()));
+    connect(hrAct, SIGNAL(triggered()), this, SLOT(newlineToBr()));
+    connect(parAct, SIGNAL(triggered()), this, SLOT(paragraphMode()));
+    connect(tableInsAct, SIGNAL(triggered()), this, SLOT(tableIns()));
 
     QFile file ("./lasttext.txt");
     if (file.open(QIODevice::ReadOnly))
     {
         QByteArray byteArray = file.readAll(); // считываем весь файл
         ui->plainTextEdit->setPlainText(tr(byteArray.data()));
+        ui->textEdit->setPlainText(tr(byteArray.data()));
         file.close();
     }
     ui->comboBox->addItem("default");
@@ -204,19 +217,135 @@ void MainWindow::read_only_switch()
     }
 }
 
+void MainWindow::newlineToBr()
+{
+    if (!htmlMode)
+    {
+        QString s = ui->textEdit->toPlainText();
+        s.replace("\n","<hr>");
+        ui->textEdit->setHtml(s);
+        hrAct->setText("Горизонтальная линия при переходе на новую строку(ВЫКЛ)");
+        htmlMode = true;
+    }
+    else
+    {
+        QString s = ui->textEdit->toPlainText();
+        s.replace("<hr>","\n");
+        ui->textEdit->setPlainText(s);
+        hrAct->setText("Горизонтальная линия при переходе на новую строку(ВКЛ)");
+        htmlMode = false;
+    }
+}
+
+void MainWindow::paragraphMode()
+{
+    if (!parMode)
+    {
+        QString s = ui->textEdit->toPlainText();
+        int c = 1;
+        s.insert(0,QString::number(c) + ". ");
+        c++;
+        for (int i=0;i<s.size();i++)
+        {
+            if (s[i] == "\n" && ((i+1) < s.size()) && s[i+1] != "\n")
+            {
+                s.insert(i+1,QString::number(c) + ". ");
+                c++;
+            }
+        }
+        ui->textEdit->setPlainText(s);
+        parMode = true;
+        parAct->setText("Нумерация абзацев(ВЫКЛ)");
+    }
+    else
+    {
+       QString s = ui->textEdit->toPlainText();
+       if (s.size() > 3 && s[0] == "1" && s[1] == ".")
+           s.remove(0,3);
+       for (int i=0;i<s.size();i++)
+       {
+           if (s[i] == "\n" && ((i+1) < s.size()) && s[i+1] != "\n")
+           {
+               if ((i+1) < s.size())
+               {
+                   while (true)
+                   {
+                       s.remove(i+1,1);
+                       if (s[i+1] == ".")
+                           break;
+                   }
+                   s.remove(i+1,2);
+               }
+           }
+       }
+       ui->textEdit->setPlainText(s);
+       parMode = false;
+       parAct->setText("Нумерация абзацев(ВКЛ)");
+    }
+}
 void MainWindow::print()
 {
+    QString printStr = ui->plainTextEdit->toPlainText();
+
+    QChar *list = printStr.data();
+    QStringList strlst;
+    int line = 0, cursor = 0;
+    for (bool getst = true;getst;)
+    {
+        int index = printStr.indexOf("\n", cursor);
+        QString s = "";
+        if (index == -1)
+        {
+           getst = false;
+           s.append(&list[cursor], printStr.length() - cursor);
+        }
+        else s.append(&list[cursor], index - cursor);
+        cursor = index + 1;
+        strlst << s;
+    }
+
     QPrinter printer;
     QPrintDialog dlg(&printer, this);
     dlg.setWindowTitle("Print");
     if (dlg.exec() != QDialog::Accepted)
         return;
+    QPainter painter;
+    painter.begin(&printer);
+    int w = painter.window().width();
+    int h = painter.window().height();
+    int amount = strlst.count();
+    QFont font = painter.font();
+    QFontMetrics fmetrics(font);
+    for (int i = 0; i < amount; i++)
+    {
+        QPointF pf;
+        pf.setX(10);
+        pf.setY(line);
+        painter.drawText(pf, strlst.at(i));
+        line += fmetrics.height();
+        if (h - line <= fmetrics.height())
+        {
+            printer.newPage();
+            line = 0;
+        }
+     }
+    painter.end();
+
+}
+
+void MainWindow::tableIns()
+{
+    int r = QInputDialog::getInt(this,"Input","rows:",2,1,100,1);
+    int c = QInputDialog::getInt(this,"Input","columns:",2,1,100,1);
+    if (r && c)
+    {
+        QTextCursor cur = ui->textEdit->textCursor();
+        QTextTable* t = cur.insertTable(r, c);
+    }
 }
 
 void MainWindow::on_comboBox_activated(const QString &arg1)
 {
-    //MainWindow *nw = qobject_cast<MainWindow*>( QApplication::topLevelWidgets()[ 0 ] );
-//QPushButton  {background-color: green;}
     if (arg1 == "green")
     {
         this->setStyleSheet("");
@@ -232,6 +361,4 @@ void MainWindow::on_comboBox_activated(const QString &arg1)
         this->setStyleSheet("");
         //nw->setStyleSheet("QPushButton {background-color: gray;}QMainWindow{background-color: gray}");
     }
-    //nw->update();
-
 }
--- les6/mainwindow.h
+++ les6/mainwindow.h
@@ -16,6 +16,8 @@ public:
     bool edit;
     int cur_undo_pos;
     bool read_only;
+    bool htmlMode;
+    bool parMode;
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
@@ -28,7 +30,9 @@ private slots:
     void on_plainTextEdit_textChanged();
     void read_only_switch();
     void print();
-
+    void newlineToBr();
+    void paragraphMode();
+    void tableIns();
     //void on_pushButton_clicked();
 
     void on_comboBox_activated(const QString &arg1);
@@ -38,5 +42,8 @@ private:
     QMenu* m_menu;
     QAction* roAct;
     QAction* printAct;
+    QAction* hrAct;
+    QAction* parAct;
+    QAction* tableInsAct;
 };
 #endif // MAINWINDOW_H
